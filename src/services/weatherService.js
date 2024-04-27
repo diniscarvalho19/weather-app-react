bb6c39175983f2bf6ba223786602cae4046e15f8
diniscarvalho19 <49685992+diniscarvalho19@users.noreply.github.com>
2024-04-27 13:00:12 +0100

    send error on retry max

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,15 +1,16 @@
 export async function getRequest(url, maxRetries = 3, retryDelay = 1000) {
   let retries = 0;
   while (retries < maxRetries) {
-      try {
-          const response = await fetch(url);
-          return await response.json();
-      } catch (error) {
-          retries++;
-          if (retries < maxRetries) {
-              await new Promise(resolve => setTimeout(resolve, retryDelay));
-          }
+    try {
+      const response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      retries++;
+      if (retries < maxRetries) {
+        await new Promise((resolve) => setTimeout(resolve, retryDelay));
       }
+      throw new Error("Error fetching data:", error);
+    }
   }
-  throw new Error('Max retries reached. Unable to fetch data.');
+  throw new Error("Max retries reached. Unable to fetch data.");
 }
